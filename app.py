a0be15ae7dcad8bb56c1c9a0cf7b884c1e6ffab3
Kaspar Peterson <kaspar.peterson@mobgen.com>
2017-11-19 15:55:28 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    html = "<center>" \
+             "<h3>My baddass webpage</h3>" \
+             "Contact me:" \
+             "<p><a href=\"https://www.linkedin.com/in/kaspar-peterson-98075994\"><b>Linked in</b></a></p>" \
+             "<p>or</p>" \
+             "<p>kasparpeterson@hotmail.com</p>" \
+           "</center>"
+    return html.format()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80)
