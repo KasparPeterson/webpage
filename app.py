8e58fda5a7f75b7f8b3d16bcbb3a976b182ce77c
KasparPeterson <kasparpeterson@hotmail.com>
2018-02-01 14:05:00 +0100

    test

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import Response
+from flask import request
 import json
 
 app = Flask(__name__)
@@ -15,13 +16,22 @@ def hello():
            "</center>"
     return html.format()
 
+
 @app.route("/weather", methods = ['POST'])
 def weather():
+    json_request = request.json
+
     text = 'This is a sample response from your webhook!'
     json_response = json.dumps({"speech": text, "displayText": text})
-    resp = Response(json_response)
+    resp = Response(json.dumps(json_request))
     resp.headers['Content-Type'] = 'application/json'
     return resp
+
+
+weather_api_endpoint = 'api.worldweatheronline.com'
+weather_api_key = 'ac171efc8a0540f7a6f120039180102'
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, threaded=True)
